80f1f798f8ad98a00403cc06071ae0c194fbdaaa
root <root@1.novalocal>
2022-06-24 08:28:13 +0700

    Inital Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: [
+        './apps/**/*.html',
+        './apps/**/*.ts',
+        './lib/**/*.html',
+        './lib/**/*.ts'
+    ],
+    darkMode: false,
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
